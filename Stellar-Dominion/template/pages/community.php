e53b88b01c1f875899cfa61faf23a12dcf62fae0
Robert Fine <thefinestrobert@gmail.com>
2025-07-28 09:03:09 -0400

    Auto‑save: community.php

--- Stellar-Dominion/template/pages/community.php
+++ Stellar-Dominion/template/pages/community.php
@@ -13,7 +13,7 @@ $page_title = 'Community';
 $active_page = 'community.php';
 
 if ($is_logged_in) {
-    require_once "lib/db_config.php";
+    require_once __DIR__ . '/../../config/config.php';
     $user_id = $_SESSION['id'];
 
     // --- DATA FETCHING ---
