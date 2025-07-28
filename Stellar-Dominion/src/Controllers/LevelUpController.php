daa18cdc3a484584b7045b7bc2d6571e827f4ae3
Robert Fine <thefinestrobert@gmail.com>
2025-07-28 00:03:06 -0400

    Auto‑save: LevelUpController.php

--- Stellar-Dominion/src/Controllers/LevelUpController.php
+++ Stellar-Dominion/src/Controllers/LevelUpController.php
@@ -2,7 +2,7 @@
 session_start();
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){ header("location: index.html"); exit; }
 
-require_once __DIR__ . '/../../lib/db_config.php'; // Corrected Path
+require_once __DIR__ . '/../../config/config.php'; // Corrected Path
 
 
 // Get points to spend from POST data
